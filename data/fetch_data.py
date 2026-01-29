abb2f5d098aa5e8d5aee787c2f521c17b83275e5
audentatler <s968410@student.knoxschools.org>
2026-01-29 21:24:51 +0000

    Attempted to import data

--- data/fetch_data.py
+++ data/fetch_data.py
@@ -1,16 +1,46 @@
+from ucimlrepo import fetch_ucirepo 
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+#...........................................
 # Copied from UCi
 
-from ucimlrepo import fetch_ucirepo 
+
+
+def get_dataX():  
+    # fetch dataset 
+    fertility = fetch_ucirepo(id=244) 
   
-# fetch dataset 
-fertility = fetch_ucirepo(id=244) 
+    # data (as pandas dataframes) 
+    X = fertility.data.features 
   
-# data (as pandas dataframes) 
-X = fertility.data.features 
-y = fertility.data.targets 
+    # metadata 
+    print(fertility.metadata) 
   
-# metadata 
-print(fertility.metadata) 
+    # variable information 
+    print(fertility.variables) 
+
+    return X
+#...........................................
+
+def get_dataY():  
+    # fetch dataset 
+    fertility = fetch_ucirepo(id=244) 
   
-# variable information 
-print(fertility.variables) 
+    # data (as pandas dataframes) 
+    y = fertility.data.targets 
+  
+    # metadata 
+    print(fertility.metadata) 
+  
+    # variable information 
+    print(fertility.variables) 
+
+    return y
+#...........................................
+
+x = get_dataX()
+y = get_dataY()
+plt.plot(x, y)
+plt.show()
